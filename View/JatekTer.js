71e3610629e669482c4e7e7bdf191bf71652cdad
patrikharman <patrikemailcim@gmail.com>
2024-09-24 14:05:43 +0200

    jatekTer szerkesztésee

---
+++ View/JatekTer.js
@@ -0,0 +1,24 @@
+import { Lampa } from "./Lampa.js";
+
+export class JatekTer {
+    #szuloElem;
+    #meret;
+    #lepes;
+    #db;
+    constructor(szuloElem) {
+        this.#meret = 3;
+        this.#lepes = 0;
+        this.#db = 0;
+        this.#szuloElem = szuloElem;
+        this.#kiir();
+    }
+
+    #kiir() {
+        const jatekTer = this.#szuloElem
+        jatekTer.empty();
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
+    }
+}
